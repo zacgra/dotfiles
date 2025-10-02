edd2d8ba0b43b2adb8b10ccae4a8fb9e4b98eb73
Zach Grammon <95185795+zacgra@users.noreply.github.com>
2025-10-02 11:30:11 -0600

    remove neotree/netrw default

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,6 +3,10 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.g.have_nerd_font = true
 
+-- Disable netrw (use neotree instead)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require 'options'
 require 'keymaps'
 require 'lazy-bootstrap'
