ac256579687171825c591343647a7c9b81dbb13e
Zach Grammon <95185795+zacgra@users.noreply.github.com>
2025-10-02 11:22:24 -0600

    view hidden files in neotree

--- config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,11 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true, -- Show hidden files by default
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
